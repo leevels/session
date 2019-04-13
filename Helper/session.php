df3143887a595812b63de5935d2c0139fa83fd9f
Xiangmin Liu <635750556@qq.com>
2019-04-13 13:37:50 +0800

    feat:  rename helper func

---
+++ Helper/session.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the ************************ package.
+ * _____________                           _______________
+ *  ______/     \__  _____  ____  ______  / /_  _________
+ *   ____/ __   / / / / _ \/ __`\/ / __ \/ __ \/ __ \___
+ *    __/ / /  / /_/ /  __/ /  \  / /_/ / / / / /_/ /__
+ *      \_\ \_/\____/\___/_/   / / .___/_/ /_/ .___/
+ *         \_\                /_/_/         /_/
+ *
+ * The PHP Framework For Code Poem As Free As Wind. <Query Yet Simple>
+ * (c) 2010-2019 http://queryphp.com All rights reserved.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Leevel\Session\Helper;
+
+use Leevel\Leevel\App;
+
+/**
+ * 设置或者获取 session 值
+ *
+ * @param null|array|string $key
+ * @param mixed             $defaults
+ *
+ * @return mixed
+ */
+function session($key = null, $defaults = null)
+{
+    $service = App::singletons()->make('sessions');
+
+    if (null === $key) {
+        return $service;
+    }
+
+    if (is_array($key)) {
+        return $service->put($key);
+    }
+
+    return $service->get($key, $defaults);
+}
